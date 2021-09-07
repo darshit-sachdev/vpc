444edde15dffed0c8dcdfb6b0121f6ffc8e84948
darshit-sachdev <89072790+darshit-sachdev@users.noreply.github.com>
2021-09-07 16:07:30 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,14 +1,11 @@
 variable "CIDR_BLOCK" {
     type = string 
-    default = "10.0.0.0/16"
 }
 
 variable "CIDR_BLOCK_SUBNET" {
     type = string
-    default = "10.0.1.0/16"
 }
 
 variable "AZ" {
     type = string
-    default = "us-east-2a"
 }
